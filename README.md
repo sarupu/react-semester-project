715d23490a46b72ec29cbf755a8c3cc01b24f6b9
sarupu <sarp.tolunay@gmail.com>
2022-11-14 22:48:46 +0300

    corrections

--- README.md
+++ README.md
@@ -1,15 +1,39 @@
 # Movie Seat Booking Site Project
 
-I created a site that you can select a movie and seat to book.
+I created a site that you can browse movies and select seats to book.
+
+This web app built with using [Vite](https://vitejs.dev/) + [React](https://reactjs.org/).
 
 Used [TMDB](https://www.themoviedb.org/) API to get the data i needed.
 
-I hid the api keys so you might not use the site right now. But here below are some images of the site experience.
+### Project Details
 
-## Images
+- [React](https://reactjs.org/)
+- [Vite](https://vitejs.dev/)
+- [Axios](https://axios-http.com/docs/intro) for making API calls
+
+### Images
 
 ![Home Page](./src/assets/icons/home.png)
 ![Responsive Home Page](./src/assets/icons/home_responsive.png)
 ![Detail Page](./src/assets/icons/detail.png)
 ![Responsive Detail Page](./src/assets/icons/detail_responsive.png)
 ![Seat Selection](./src/assets/icons/seatsEtc.png)
+
+### Install dependencies
+
+```
+npm install
+```
+
+### Run Vite dev server
+
+```
+npm run dev
+```
+
+### To build for production
+
+```
+npm run build
+```
--- src/components/Section/Row/Row.jsx
+++ src/components/Section/Row/Row.jsx
@@ -4,23 +4,29 @@ import useFetch from "../../../hooks/use-fetch"
 import { img_url } from "../../../hooks/use-fetch"
 import "./Row.scss"
 
-
 const Row = ({ title }) => {
-
   const { movies, getPosters } = useFetch()
 
   //Called the function here.
   useEffect(() => {
     getPosters(title)
-    console.log(movies)
   }, [title])
 
   return (
     <div className="row">
       <h2>{title}</h2>
-      <div className={`row__posters ${title === "Now Showing" ? "scroll" : false}`} >
-        {movies?.map((movie) => 
-        <NavLink key={movie?.id} state={title === "Now Showing" ? true : false} to={`/detail/${movie.id}`}><img src={`${img_url}${movie?.poster_path}`} /></NavLink>)}
+      <div
+        className={`row__posters ${title === "Now Showing" ? "scroll" : false}`}
+      >
+        {movies?.map((movie) => (
+          <NavLink
+            key={movie?.id}
+            state={title === "Now Showing" ? true : false}
+            to={`/detail/${movie.id}`}
+          >
+            <img src={`${img_url}${movie?.poster_path}`} />
+          </NavLink>
+        ))}
       </div>
     </div>
   )
